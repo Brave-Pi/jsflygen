ac5e55c53501aff7674db3e7a89cdc0ed0d5f34f
Gabriel Hayes <gabriel.speed.bullock@gmail.com>
2020-07-05 10:31:15 -0500

    Initial commit.

---
+++ src/JsFlyGenerator.hx
@@ -0,0 +1,55 @@
+package;
+
+import haxe.macro.Type;
+import haxe.macro.Expr;
+import haxe.macro.*;
+
+#if macro
+using tink.MacroApi;
+#end
+
+import test.Test;
+import StringTools;
+
+class JsFlyGenerator {
+	#if macro
+	@:persistent static var gen:ExampleJSGenerator;
+	@:persistent static var val:Int;
+
+	public static function use() @:privateAccess{
+		val = 5;
+		Compiler.setCustomJSGenerator(api -> {
+            gen = new ExampleJSGenerator(api);
+            trace(gen);
+            var cl = macro haxe.io.Bytes.ofString('');
+            trace(cl);
+			var type = cl.typeof().sure();
+			trace(type);
+			gen.genType(type);
+			sys.io.File.saveContent("./bytes.js", gen.buf.toString());
+			while (true) {
+				Sys.sleep(1.0);
+			}
+		});
+	}
+	#end
+
+	public static macro function test()  {
+		trace(gen);
+		trace(val);
+		// var cl = macro(new Test());
+		// var type = cl.typeof().sure();
+		// trace(type);
+		// gen.genType(type);
+		// trace(gen.buf.toString());
+		// while (true) {
+		// 	Sys.sleep(1.0);
+		// }
+
+		return macro {}
+	}
+
+	public static function main() {
+		test();
+	}
+}
---
+++ src/test/Test.hx
@@ -0,0 +1,12 @@
+package test;
+
+
+
+class Test {
+    public function new() {
+
+    }
+    public function test() {
+        trace('test');
+    }
+}
